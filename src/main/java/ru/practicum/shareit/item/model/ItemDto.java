424a29b60d5237d0d5d4bffeeb7e70470af9b9bf
нфÐyarichekp <yanormik@gmail.com>
2025-01-17 18:25:16 +0300

    Отправка на второе ревью

--- src/main/java/ru/practicum/shareit/item/model/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/model/ItemDto.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.item.model;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
