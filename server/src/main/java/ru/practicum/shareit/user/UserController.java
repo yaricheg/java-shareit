0357f70faae0c56463aea717508dfe3756da640c
нфÐyarichekp <yanormik@gmail.com>
2025-03-05 14:18:25 +0300

    Изменение шаблона

---
+++ server/src/main/java/ru/practicum/shareit/user/UserController.java
@@ -0,0 +1,55 @@
+package ru.practicum.shareit.user;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.user.model.UserDto;
+import ru.practicum.shareit.user.service.UserService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(path = "/users")
+@RequiredArgsConstructor
+@Slf4j
+public class UserController {
+
+
+    @Autowired
+    private final UserService userService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDto createUser(@Valid @RequestBody UserDto userDto) {
+        return userService.createUser(userDto);
+    }
+
+    @PatchMapping("/{userId}")
+    @ResponseStatus(HttpStatus.OK)
+    public UserDto updateUser(@PathVariable("userId") long userId, @RequestBody UserDto userDto) {
+        return userService.updateUser(userId, userDto);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<UserDto> getUsers() {
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{userId}")
+    @ResponseStatus(HttpStatus.OK)
+    public UserDto getUserById(@PathVariable("userId") long userId) {
+        return userService.getUserById(userId);
+    }
+
+    @DeleteMapping("/{userId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable("userId") long userId) {
+        userService.deleteUser(userId);
+    }
+
+}
---
+++ server/src/main/java/ru/practicum/shareit/user/model/UserDto.java
@@ -0,0 +1,18 @@
+package ru.practicum.shareit.user.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserDto {
+
+    private long id;
+
+    private String name;
+
+    private String email;
+
+}
